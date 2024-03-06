7c45600a9f3c15d2759b24a758790a6efc9f20ee
Antonio Ortiz <tonitoortiz17@gmail.com>
2024-03-06 16:27:06 +0100

    subida

--- src/app/page.js
+++ src/app/page.js
@@ -29,19 +29,3 @@ export default function Home() {
     </>
   );
 }
-
-// function Comprobar() {
-//   if (!session) {
-//     <Link href="/auth/login">
-//       <svg className="h-20 w-20 text-orange-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-//       </svg>
-//     </Link>
-//   } else {
-//     <Link href="/dashboard">
-//       <svg className="h-20 w-20 text-orange-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-//       </svg>
-//     </Link>
-//   }
-// }
